2983e467e5a56ec41de4b1ff23034e405e919ad2
Alexander Kabanov <kabanov@live.com>
2025-03-21 00:47:21 +0400

    code changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
